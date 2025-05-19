729173278a4b27338edcb3826442ba0b5644f700
Manisumanth <manisumanthk@gmail.com>
2025-05-19 19:51:46 +0530

    updated

---
+++ p1/p3/script.js
@@ -0,0 +1,5 @@
+const customer = [
+    {cart:"1234567890",name:"Bob",pin:"1234",balance: 0},
+    {cart:"1234567891",name:"Chris",pin:"1231",balance: 10},
+    {cart:"1234567892",name:"Henry",pin:"1234",balance: 20},
+]
